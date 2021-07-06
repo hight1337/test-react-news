88ae93678b61caa8389d01d8d67324b73b15ec62
Vlad <twister.09@mail.ru>
2021-07-06 17:47:21 +0300

    deleted all unnecessary

---
+++ src/pages/newsPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const newsPage = () => {
+  return <div></div>
+}
+
+export default newsPage
---
+++ src/pages/profilePage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const profilePage = () => {
+  return <div></div>
+}
+
+export default profilePage
