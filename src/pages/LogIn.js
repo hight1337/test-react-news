5349dc69867f14b94137b11bc7d2226b889d9801
Vlad <twister.09@mail.ru>
2021-07-07 18:10:01 +0300

    aded redirect from login page to profile

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import LogInFrom from '../components/LogInFrom'
+import Login from '../store/Login'
+import { Redirect } from 'react-router'
 
 const LogIn = () => {
-  return (
-    <div>
-      <LogInFrom />
-    </div>
-  )
+  return <div>{Login.login ? <Redirect to='/profile' /> : <LogInFrom />}</div>
 }
 
 export default LogIn
